6515efbadd2926046c03d43f2c9b751cd7b5a731
Nusrat Doula Eshan <65530543+EshanDdos@users.noreply.github.com>
2021-06-10 22:33:27 +0600

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,40 @@
+#total three types of data available in python they are 1)numeric(integer,float,complex,bool), 2)sequence(string,list,set,touple,range) 3) dictionary
+#numeric
+'''x = 4
+y = type(x)
+print(y)'''
+'''x = 4.5
+y = type(x)
+print(y)'''
+'''x = 4.5 + 5j
+y = type(x)
+print(y)'''
+'''x = 4.5 + 8j
+y = type(x)
+print(y)'''
+'''x = 4.5
+m = 5.5
+y = type(x>m)
+print(y)'''
+#sequence
+#string
+'''x = 'Eshan'
+y = type(x)
+print(y)'''
+#list
+'''x = [1,2,3,4,5,6]
+y = type(x)
+print(y)'''
+#tuple
+'''x = (1,2,3,4,5,6)
+y = type(x)
+print(y)'''
+#set
+'''x = {1,2,3,4,5,6}
+y = type(x)
+print(y)'''
+#range
+x = [1,2,3,4,5,6]
+y = type(x)
+print(y)
+
